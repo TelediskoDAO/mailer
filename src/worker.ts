4032ab6dfed9fad881eae0446034a3bbf40b5592
Nicola Miotto <miotto@posteo.de>
2022-02-25 12:43:22 +0100

    add skeleton for mailing function

--- src/worker.ts
+++ src/worker.ts
@@ -1,8 +1,9 @@
 //import { handleRequest } from './handler'
 import { ethers } from 'ethers'
 
+const ZOHO_API = 'https://mail.zoho.com/api/accounts/<accountId>/messages'
+const ZOHO_AUTH_TOKEN = 'noidea'
 const CONTRACT_ADDRESS = '0x3E3fCa9Da850E22495590b7482043ad61e24CE09'
-
 const ABI = [
   'event ResolutionCreated(address indexed from, uint256 indexed resolutionId)',
 ]
@@ -17,20 +18,35 @@ async function handleRequest(request: Request): Promise<Response> {
   var filter = contract.filters.ResolutionCreated()
   let events = await contract.queryFilter(filter, 10203587, 'latest')
 
-  var messages: string[] = []
-  events.forEach((event) => {
-    if (event.args !== undefined) {
-      const content = event.args
+  var results: string[] = []
+  await Promise.all(
+    events.map(async (event) => {
+      if (event.args !== undefined) {
+        const content = event.args
 
-      const address = content[0].toString()
-      const resolutionId = content[1].toString()
-      messages.push(
-        `Address ${address} created pre draft with id ${resolutionId}`,
-      )
-    }
-  })
+        const address = content[0].toString()
+        const resolutionId = content[1].toString()
+        const mailBody = {
+          fromAddress: 'mailer@teledisko.com',
+          toAddress: 'miotto@posteo.de',
+          subject: 'New Pre-Draft to Review',
+          content: `Hi Benjamin, the wallet ${address} has created a new pre-draft. Would you mind reviewing it at https://dao.teledisko.com/resolutions/${resolutionId}.`,
+          askReceipt: 'yes',
+        }
+        const result = await fetch(ZOHO_API, {
+          body: JSON.stringify(mailBody),
+          headers: {
+            Authorization: `Zoho-oauthtoken ${ZOHO_AUTH_TOKEN}`,
+            'Content-Type': 'application/json',
+          },
+          method: 'POST',
+        })
+        results.push(result.status.toString())
+      }
+    }),
+  )
 
-  return new Response(messages.join('\n'))
+  return new Response(results.join('\n'))
 }
 
 addEventListener('fetch', (event) => {
